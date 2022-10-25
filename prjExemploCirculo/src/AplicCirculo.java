40a270f09ca88b0b3ab43eee46b7bd5588130478
AlmeidMa <marianelimoli@gmail.com>
2022-10-25 15:20:27 -0300

    adicionando Decimal Format

--- prjExemploCirculo/src/AplicCirculo.java
+++ prjExemploCirculo/src/AplicCirculo.java
@@ -1,5 +1,6 @@
 
 import fatec.poo.model.Circulo;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 /**
  *
@@ -11,18 +12,21 @@ public class AplicCirculo {
     public static void main(String[] args) {
         double raio;
         
+        DecimalFormat df = new DecimalFormat("0.0");
         Scanner entrada = new Scanner(System.in);
         Circulo objCirculo = new Circulo();
         
+        System.out.println("\t---EXERCÍCIO DO CÍRCULO---");
         System.out.print("Digite o raio do circulo: ");
         raio = entrada.nextDouble();
         
         objCirculo.setRaio(raio);
         
-        System.out.println("O circulo possui um raio de " + objCirculo.getRaio());
-        System.out.println("A área é: " + objCirculo.calcArea());
-        System.out.println("O perimetro é: " + objCirculo.calcPerimetro());
-        System.out.println("O diametro é: " + objCirculo.calcDiametro());
+        
+        System.out.println("O circulo possui um raio de " +df.format( objCirculo.getRaio()));
+        System.out.println("A área é: " + df.format( objCirculo.calcArea()));
+        System.out.println("O perimetro é: " + df.format( objCirculo.calcPerimetro()));
+        System.out.println("O diametro é: " + df.format( objCirculo.calcDiametro()));
         
         
         
